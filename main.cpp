6bf3ee1a027bd31964c43b3319727c75eedc606f
Pavle Milanov <pavle_milanov@hotmail.com>
2024-04-15 00:49:44 +0200

    Sample code for looping through a directory, and calculating sizes.

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@
 #include "headers/File.h"
 #include "headers/FSItem.h"
 
-#define BIN_PATH = " ~/.local/share/Trash/";
-#define APP_CACHE_PATH = "~/.cache/";
-#define APP_LOGS = "/var/log";
+#define BIN_PATH " ~/.local/share/Trash/"
+#define APP_CACHE_PATH "~/.cache/"
+#define APP_LOGS "/var/log"
+#define DEBUG 1
 // #define APT_CACHE = "/var/cache/apt/archives";
 
 using namespace std;
 
 void clear(void);
 void delete_dir_content(const filesystem::path& dir);
+uintmax_t directory_size(string path);
 
 int main() {
 	int choice;
@@ -23,27 +25,34 @@ int main() {
 	cout << "1. Scan a directory." << endl;
 	cout << "2. Empty recycle bin." << endl;
 	cout << "3. Delete app cache." << endl;
-	cout << "4. Delete app logs." << endl;
+	cout << "4. Delete app logs. (requires root)" << endl;
 	cout << "5. Exit" << endl;
 	while (true){
 		cin >> choice;
 		//When something that is not a number is entered, it returns 0.
 		if (choice == 1) {
 			string path;
-			File f("test", "test", "test");
-			File f1("test", "test", "test");
-			Directory d1("test", "test", "test");
-			vector<FSItem*> fs;
-			fs.push_back(&f);
-			fs.push_back(&f1);
-			fs.push_back(&d1);
-			for (auto i : fs) {
-				i->del();
+			// File f("test", "test", "test");
+			// File f1("test", "test", "test");
+			// Directory d1("test", "test", "test");
+			// vector<FSItem*> fs;
+			// fs.push_back(&f);
+			// fs.push_back(&f1);
+			// fs.push_back(&d1);
+			// for (auto i : fs) {
+			// 	i->del();
+			// }
+			cout << "Enter the path: (/ for root, this might take a considerable amount of time. Root is also required to access protected parts of the filesystem.)" << endl;
+			cin >> path;
+			for (const auto & entry: filesystem::directory_iterator(path)) {
+				if (filesystem::is_directory(entry)){
+					uintmax_t s = directory_size(entry.path());
+					cout << entry.path() << " " << s << endl;
+				}
 			}
-			cout << "Enter the path: (/ for root, this might take a considerable amount of time.)" << endl;
 		}
 		else if (choice == 2) {
-			delete_dir_content(BIN_PATH);
+			// delete_dir_content(BIN_PATH);
 			cout << "Empty recycle bin" << endl;
 		}
 		else if (choice == 3) {
@@ -75,4 +84,23 @@ void delete_dir_content(const filesystem::path& dir){
 	for (const auto& entry: filesystem::directory_iterator(dir)) {
 		filesystem::remove_all(entry.path());
 	}
+}
+
+uintmax_t directory_size(string path) {
+    uintmax_t size = 0;
+    for (const auto& entry : filesystem::recursive_directory_iterator(path)) {
+        if (!filesystem::is_directory(entry)) {
+        	try{
+    	        size += filesystem::file_size(entry);
+        	}
+        	//Failed to read for various reasons... (Permissions, fs errors, etc...)
+        	catch (filesystem::filesystem_error &exc){
+        		if (DEBUG){
+	        		cout << "ERROR!" << " " << exc.what() << endl;
+        		}
+        		continue;
+        	}
+        }
+    }
+    return size;
 }
